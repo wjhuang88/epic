d04e861a5020d2e10d4964f5252716ad31c9b37f
Huang.Weijie <wjhuang@live.cn>
2020-04-02 10:03:25 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use actix_web::{HttpServer, App, web, HttpResponse};
+use actix_web::{HttpServer, App, web, HttpResponse, HttpMessage, http, FromRequest, Responder};
 use lazy_static::*;
+use futures::StreamExt;
+use mimalloc::MiMalloc;
+use git2::Repository;
+use git2::transport::Transport;
+use actix_web::dev::Factory;
+use std::future::Future;
 
 #[allow(non_snake_case)]
 mod gen___pages;
+mod logger_config;
 
 lazy_static! {
     static ref STATIC_MAP: gen___pages::GenPages<'static> = gen___pages::GenPages::new();
 }
 
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    logger_config::print_banner();
+    logger_config::init_log();
 
     HttpServer::new(|| {
         let mut app = App::new();
@@ -42,8 +54,34 @@ async fn main() -> std::io::Result<()> {
             }));
         }
         app
+            .route("{project}.git/HEAD", web::get().to(get_head))
+            .route("{project}.git/info/refs", web::get().to(get_info_refs))
+            .route("{project}.git/objects/info/alternates", web::get().to(get_text_file))
+            .route("{project}.git/objects/info/http-alternates", web::get().to(get_text_file))
+            .route("{project}.git/objects/info/packs", web::get().to(get_info_packs))
+            .route("{project}.git/objects/{pre}/{id}", web::get().to(get_loose_object))
+            .route("{project}.git/objects/pack/pack-{id}\\.pack", web::get().to(get_pack_file))
+            .route("{project}.git/objects/pack/pack-{id}\\.idx", web::get().to(get_idx_file))
+
+            .route("/git-upload-pack", web::post().to(service_rpc))
+            .route("/git-receive-pack", web::post().to(service_rpc))
     })
         .bind("127.0.0.1:8000")?
         .run()
         .await
 }
+
+async fn get_head(req: web::HttpRequest) -> impl Responder {
+    let maybe_project = req.match_info().get("project");
+    let git_repo = Repository::open("demo-git-repo").unwrap();
+    println!("{}", maybe_project.unwrap());
+    "get_head"
+}
+async fn get_info_refs(req: web::HttpRequest) -> impl Responder { "get_info_refs" }
+async fn get_text_file(req: web::HttpRequest) -> impl Responder { "get_text_file" }
+async fn get_info_packs(req: web::HttpRequest) -> impl Responder { "get_info_packs" }
+async fn get_loose_object(req: web::HttpRequest) -> impl Responder { "get_loose_object" }
+async fn get_pack_file(req: web::HttpRequest) -> impl Responder { "get_pack_file" }
+async fn get_idx_file(req: web::HttpRequest) -> impl Responder { "get_idx_file" }
+
+async fn service_rpc(req: web::HttpRequest) -> impl Responder { "service_rpc" }
